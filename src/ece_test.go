7b0e4446ec20b493777bccb02b680c81f6fd3523
JulesDELTEIL <jules.delteil@epitech.eu>
2026-09-10 14:47:11 +0900

    feat(testing): add testing functions

--- src/ece_test.go
+++ src/ece_test.go
@@ -1,11 +1,32 @@
 package ece
 
-import "fmt"
+import (
+	"testing"
+)
 
-func main() {
+func TestComponentRegistration(t *testing.T) {
+	reg := NewRegistry()
+	RegisterComponents[int](reg)
+	integers := GetComponents[int](reg)
+	if integers == nil {
+		t.Error("error: could not get registred component type 'int'")
+	}
+	nothing := GetComponents[string](reg)
+	if nothing != nil {
+		t.Error("error: huh ?")
+	}
+}
+
+func TestAddingComponent(t *testing.T) {
 	reg := NewRegistry()
 	RegisterComponents[int](reg)
 	integers := GetComponents[int](reg)
 	AddComponent[int](reg, 0, 5)
-	fmt.Println(integers.Get(0))
+	val, err := integers.Get(0)
+	if err != nil {
+		t.Errorf(`error: trying to get component from entity: %v`, err)
+	}
+	if val != 5 {
+		t.Error("error: wrong value from getting component")
+	}
 }

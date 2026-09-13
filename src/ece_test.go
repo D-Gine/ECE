7ef3a6be526027d6cea768a1cc02e0b488c2e2a7
JulesDELTEIL <jules.delteil@epitech.eu>
2026-09-13 12:54:57 +0900

    feat: tests for sparse array insertion and deletion

--- src/ece_test.go
+++ src/ece_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 )
 
+const test_entity int = 10
+
 func TestComponentRegistration(t *testing.T) {
 	reg := NewRegistry()
 	if reg == nil {
@@ -24,12 +26,33 @@ func TestAddingComponent(t *testing.T) {
 	reg := NewRegistry()
 	RegisterComponents[int](reg)
 	integers := GetComponents[int](reg)
-	AddComponent[int](reg, 0, 5)
-	val, err := integers.Get(0)
+
+	AddComponent[int](reg, test_entity, 5)
+	val, err := integers.Get(test_entity)
 	if err != nil {
 		t.Errorf(`error: trying to get component from entity: %v`, err)
 	}
 	if val != 5 {
 		t.Error("error: wrong value from getting component")
+	}
+}
+
+func TestRemovingComponent(t *testing.T) {
+	reg := NewRegistry()
+	RegisterComponents[int](reg)
+	integers := GetComponents[int](reg)
+
+	AddComponent[int](reg, test_entity, 5)
+	_, err := integers.Get(test_entity)
+	if err != nil {
+		t.Errorf(`error: Get component not working: can't test remover`)
+	}
+	err = integers.Remove(test_entity)
+	if err != nil {
+		t.Errorf(`error: could not remove component: %v`, err)
+	}
+	_, err = integers.Get(test_entity)
+	if err == nil {
+		t.Error("error: component not removed")
 	}
 }
